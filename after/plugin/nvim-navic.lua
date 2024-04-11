3362ff8e2bf1a39c77478310e59f79832ed0d489
Filip Kaksa <filip.kaksa@adcubum.com>
2024-04-11 15:17:37 +0200

    Added keymap for copying breadcrumb to a clipboard

--- after/plugin/nvim-navic.lua
+++ after/plugin/nvim-navic.lua
@@ -1,4 +1,5 @@
 local navic = require('nvim-navic')
+local keymap = vim.keymap
 
 navic.setup {
   icons = {
@@ -31,3 +32,23 @@ navic.setup {
   },
   click = true
 }
+
+-- copy breadcrumb to clipboard
+keymap.set('n', '<leader>yb',
+  function()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local breadcrumb = ''
+    if navic.is_available(bufnr) then
+      local navic_data = navic.get_data(bufnr)
+      for _, comp in ipairs(navic_data) do
+        -- if comp.name contains '.' then put '' around it
+        if string.find(comp.name, '%.') then
+          comp.name = '\'' .. comp.name .. '\''
+        end
+        breadcrumb = breadcrumb .. '.' .. comp.name
+      end
+    end
+    vim.fn.setreg('*', breadcrumb)
+  end,
+  { noremap = true, silent = true, desc = 'Copy breadcrumb to clipboard'}
+)
