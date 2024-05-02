c27f8582b515e074deb3ed559249e99c55f3c2d7
Filip Kaksa <filip.kaksa@adcubum.com>
2024-05-02 14:07:01 +0200

    Telegraf added multi select

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,11 +3,31 @@ local telescope = require('telescope')
 local trouble = require('trouble')
 local keymap = vim.keymap
 
+-- function copied from https://github.com/nvim-telescope/telescope.nvim/issues/1048#issuecomment-1679797700
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require('telescope.actions').close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        vim.cmd(string.format('%s %s', 'edit', j.path))
+      end
+    end
+  else
+    require('telescope.actions').select_default(prompt_bufnr)
+  end
+end
+
 telescope.setup {
   defaults = {
     mappings = {
-      i = { ["<c-t>"] = trouble.open_with_trouble },
-      n = { ["<c-t>"] = trouble.open_with_trouble },
+      i = {
+        ['<CR>'] = select_one_or_multi,
+      },
+      n = {
+        ['<CR>'] = select_one_or_multi,
+      }
     },
   },
   extensions = {
