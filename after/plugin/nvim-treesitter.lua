2c0da422849c434f910f893132ee351a1d06224b
fkaksa <fkfilip9@gmail.com>
2025-07-21 13:43:08 +0200

    fixed nvim-treesitter

--- after/plugin/nvim-treesitter.lua
+++ after/plugin/nvim-treesitter.lua
@@ -1,22 +1,26 @@
-local configs = require("nvim-treesitter")
+local treesitter = require("nvim-treesitter")
 
-configs.setup({
-  install = {
-    "lua",
-    "bash",
-    "vim",
-    "vimdoc",
-    "query",
-    "html",
-    "go",
-    "bash",
-    "yaml",
-    "json",
-    "markdown_inline",
-    "terraform",
-    "kotlin",
-  },
-  sync_install = false,
-  highlight = { enable = true },
-  indent = { enable = true },
+treesitter.setup {
+  -- Directory to install parsers and queries to
+  install_dir = vim.fn.stdpath('data') .. '/site'
+}
+treesitter.install {
+  "lua",
+  "bash",
+  "vim",
+  "vimdoc",
+  "query",
+  "html",
+  "go",
+  "bash",
+  "yaml",
+  "json",
+  "markdown_inline",
+  "terraform",
+  "kotlin",
+}
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '*' },
+  callback = function() vim.treesitter.start() end,
 })
--- lsp/terraformls.lua
+++ lsp/terraformls.lua
@@ -1,5 +1,6 @@
 vim.lsp.config('terraformls', {
   cmd = { "terraform-ls", "serve", "-req-concurrency", "20" },
+  filetypes = { "terraform", "terraform-vars" },
   capabilities = {
     textDocument = {
       completion = {
